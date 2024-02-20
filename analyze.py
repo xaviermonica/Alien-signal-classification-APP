bad3378cd1c82b6e2bf2b96eed4973aaa9888a33
xaviermonica <xaviermonicaa123@gmail.com>
2024-02-20 13:41:00 +0530

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -25,7 +25,7 @@ st.markdown("""
 
     /* Style headings with bold and color */
     h1, h2, h3, h4, h5, h6 {
-        color: #42a8db;
+        color: #9cd4f0;
         font-weight: 700;
         font-family: 'Arial', sans-serif;
     }
