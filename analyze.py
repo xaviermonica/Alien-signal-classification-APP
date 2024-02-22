79b262156969743ee0ec14af34f674cf57f5255b
xaviermonica <xaviermonicaa123@gmail.com>
2024-02-22 17:06:30 +0530

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -154,7 +154,7 @@ if anova_columns:
     import statsmodels.api as sm
 
     for col in anova_columns:
-        model = ols(f'{col} ~ C(Stars Type)', data=data).fit()
+        model = ols(f'{col} ~ C(`Stars Type`)', data=data).fit()
         anova_table = sm.stats.anova_lm(model, typ=2)
         st.write(f"#### ANOVA Results for {col}")
         st.write(anova_table)
