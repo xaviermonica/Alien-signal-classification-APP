8de657b149e3131632e30e1739275a82a8ce95f2
xaviermonica <xaviermonicaa123@gmail.com>
2024-02-22 20:32:12 +0530

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 from scipy import stats
-
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
 st.title("📊 Advanced Data Analysis of Narrowband Signals")
 
 # Load the narrowband signals data
